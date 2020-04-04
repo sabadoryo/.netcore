373fa84fdef12f235a4f8734ade1cfed30a25846
sabadoryo <eabilbay@gmail.com>
2020-04-04 13:48:43 +0600

    practice 2 -> done

---
+++ WebApplication1/Controllers/MessagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MessagesController : Controller
+    {
+        [Route("{**message}")]
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
