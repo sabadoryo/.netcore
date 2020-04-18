378b33c55d8ba717c5de067c64ab3df894669136
sabadoryo <eabilbay@gmail.com>
2020-04-18 12:30:00 +0600

    tutorial 4

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -16,6 +18,10 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MoviesContext>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
